27dde55da7483a7a7a5f94ceee78927b4c04429f
Acklas Banji <acklas2007@gmail.com>
2026-09-01 21:53:34 +0200

    Created Class, Constructor, used Encapsulation and added methods

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -1,3 +1,51 @@
+
+class Account{
+    private int accountNumber;
+    private String accountHolder;
+    private double deposit;
+    private double balance;
+
+    Account(int accountNumber, String accountHolder, double deposit, double balance){
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber; 
+        this.balance = deposit;
+    }
+    public int getNumber(){
+        return accountNumber;
+    }
+
+    public String getHolder(){
+        return accountHolder;
+    }
+
+    public double getDeposit(){
+        return deposit;
+    }
+    public double getBalance(){
+        return balance;
+    }
+
+    public void Deposit(double amount){
+        if(amount < 0){
+            System.out.println("Invalid Amount to Deposit...");
+            return;
+        }
+        balance += amount;
+        System.out.println("Money Deposited Successfully...");
+        System.out.println("New Balance: " + balance);
+    }
+
+    public void Withdraw(double amount){
+        if(amount > balance){
+            System.out.println("Insufficient Funds to make Withdraw...");
+            return;
+        }
+        balance -= amount;
+        System.out.println("Money Withdrawn Successfully...");
+        System.out.println("New Balance: " + balance);
+    }
+}
+
 public class BankManagementSystem {
     
 }
