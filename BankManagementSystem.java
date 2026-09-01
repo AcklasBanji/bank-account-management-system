191cd854eb34ad593393eaea0f792590466cbe7a
Acklas Banji <acklas2007@gmail.com>
2026-09-01 22:25:31 +0200

    Added Menu and switch

--- BankManagementSystem.java
+++ BankManagementSystem.java
@@ -1,4 +1,7 @@
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 class Account{
     private int accountNumber;
     private String accountHolder;
@@ -47,5 +50,67 @@ class Account{
 }
 
 public class BankManagementSystem {
-    
+    public static void main(String [] args){
+        ArrayList<Account> Database = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        boolean running = true;
+        while(running){
+            System.out.println("\t\nBANK ACCOUNT MANAGEMENT SYSTEM");
+            System.out.println("1. Create Account");
+            System.out.println("2. Deposit Money");
+            System.out.println("3. Withdraw Money");
+            System.out.println("4. Check Balance");
+            System.out.println("5. Display Account");
+            System.out.println("6. Search Account");
+            System.out.println("7. Display All Accounts");
+            System.out.println("8. Delete Account");
+            System.out.println("9. Transaction History");
+            System.out.println("10.Transfer Money");
+            System.out.println("11. Exit");
+
+            System.out.print("Enter Option: ");
+            String choice = input.nextLine();
+
+            switch(choice){
+                case "1":
+
+                break;
+                case "2":
+
+                break;
+                case "3":
+
+                break;
+                case "4":
+
+                break;
+                case "5":
+
+                break;
+                case "6":
+
+                break;
+                case "7":
+
+                break;
+                case "8":
+
+                break;
+                case "9":
+
+                break;
+                case "10":
+
+                break;
+                case "11":
+                    running = false;
+                    System.out.println("Exiting...");
+
+                break;
+                default:
+                    System.out.println("Inavlid Option...");
+            }
+        }    
+    }
 }
